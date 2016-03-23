4a063294ae1e782ff2bf079830021bedffb2ac3a
Hill <hill@kimo.com>
2016-03-23 18:05:49 +0000

    improve output

--- debug_cli.php
+++ debug_cli.php
@@ -2,7 +2,7 @@
 namespace PMVC\PlugIn\debug;
 use PMVC as p;
 ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\debug_cli';
-p\initPlugin(['debug'=>null]);
+p\initPlugin(['debug'=>null], true);
 
 class debug_cli
     extends p\PlugIn
@@ -11,18 +11,38 @@ class debug_cli
     public function init()
     {
         if (!isset($this['level'])) {
-            $this['level'] = p\plug('debug')->getLevel('debug');
+            $this['level'] = 'debug';
         }
     }
 
     public function escape($s) { return $s; }
 
+    public function getColor($level)
+    {
+        $levels =  [
+            'trace'=>'%y',
+            'debug'=>'%g',
+            'info'=>'%b',
+            'warn'=>'%w',
+            'error'=>'%r'
+        ];
+        if (isset($levels[$level])) {
+            return $levels[$level];
+        } else {
+            return null;
+        }
+    }
+
     public function dump($p, $type='debug')
     {
         $cli = p\plug('cli');
         $debug = p\plug('debug');
-        if ($debug->getLevel($type) >= $this['level']) {
-            $cli->dump($p);
+        if ($debug->getLevel($type) >= $debug->getLevel($this['level'])) {
+            if (!is_array($p)) {
+                $cli->dump($p, $this->getColor($type));
+            } else {
+                $cli->tree($p, $this->getColor($type));
+            }
         }
     }
 }
